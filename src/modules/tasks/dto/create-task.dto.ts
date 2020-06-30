05d03fad915f03823897ff59651a71319e312b92
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-30 23:45:37 +0300

    update: add api docs decorators

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, IsBoolean, IsOptional, IsNotEmpty } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateTaskDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   text: string;
 
   @IsBoolean()
   @IsOptional()
+  @ApiPropertyOptional({ default: false })
   isDone: boolean;
 }
