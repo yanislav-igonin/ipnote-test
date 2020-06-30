dde06210c569e4842453d5bf7d96be6351b7a038
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-30 20:28:07 +0300

    add: config file

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+const { PORT, NODE_ENV, MONGO_URL } = process.env;
+
+const config = {
+  port: PORT ? parseInt(PORT, 10) : 3000,
+  env: NODE_ENV || 'development',
+  mongoUrl: MONGO_URL || 'mongodb://localhost/test'
+}
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(config.port);
 }
 bootstrap();
