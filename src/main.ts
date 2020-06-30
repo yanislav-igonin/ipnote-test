668a3f9abc52f4060e74f119ff73c462e3b512d4
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-30 19:58:45 +0300

    update: swagget description

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('Tasks example')
+    .setDescription('The tasks API description')
+    .setVersion('0.1')
+    .addTag('tasks')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
