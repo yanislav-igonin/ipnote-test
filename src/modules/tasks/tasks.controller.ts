1c7a5d3a2fdc2f74ef586bc980936787e45246cd
Yanislav Igonin <yanislavdabest@gmail.com>
2020-06-30 21:45:31 +0300

    update: controller and service

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Param, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './schemas/task.schema';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  async getAll(@Param('search') search: string): Promise<{ tasks: Task[] }> {
+    const tasks = await this.tasksService.getAll(search);
+    return { tasks };
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<{ task: Task }> {
+    const createdTask = await this.tasksService.create(task);
+    return { task: createdTask };
+  }
+}
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './schemas/task.schema';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  async getAll(search: string): Promise<Task[]> {
+    return this.taskModel.find({ text: search }).exec();
+  }
+
+  async create(createTaskDto: Task): Promise<Task> {
+    const createdTask = new this.taskModel(createTaskDto);
+    return createdTask.save();
+  }
+}
